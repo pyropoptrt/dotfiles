e14ada53b26cd253e863ea87a28147e637d619b3
pyropoptrt <lee@pyropoptrt.com>
2016-03-15 09:56:52 -0500

    Brew-cask is no longer used so our references have been updated

--- homebrew/cask.sh
+++ homebrew/cask.sh
@@ -2,11 +2,12 @@
 
 # Install native apps
 # make sure that brew cask is installed first
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
+# brew install caskroom/cask/brew-cask
+# brew tap caskroom/versions
 
 # to maintain cask ....
-brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+# brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+brew update && brew cleanup && brew cask cleanup
 
 # daily
 brew cask install spectacle

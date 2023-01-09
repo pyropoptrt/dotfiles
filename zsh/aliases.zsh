09600a5ed8432499effd08f7eea6d4a0948c7098
pyropoptrt <lee@pyropoptrt.com>
2023-01-09 16:58:50 -0600

    Shortcuts for navigating my system

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,17 @@
 alias reload!='. ~/.zshrc'
+
+
+# file listing
+alias ll="ls -lHp --color=auto"
+alias la="ls -lAaHp --color=auto"
+alias ltr="ls -ltrHp | tail"
+
+
+alias x="exit"
+
+# Quick folder navigation
+alias desk="cd ~/Desktop"
+alias down="cd ~/Downloads"
+alias notes="st ~/Dropbox/Unix\ Notes.txt"
+alias epic="~/Desktop/Epic/files"
+

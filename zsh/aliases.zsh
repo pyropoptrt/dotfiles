b8b37509f34cdde1ab33b65a2fafd5560d7ec6e2
pyropoptrt <lee@pyropoptrt.com>
2023-01-09 16:59:04 -0600

    Fixes for application crashes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,6 +9,15 @@ alias ltr="ls -ltrHp | tail"
 
 alias x="exit"
 
+
+# Fix for when quicksilver crashes
+alias quick="killall Quicksilver >> /dev/null; open /Applications/QuickSilver.app"
+
+
+# Fix for when CoreAudio crashes
+# alias coreaudio="sudo launchctl kickstart -kp system/com.apple.audio.coreaudio; sudo killall 'AU Lab' >> /dev/null; open ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/Dropbox\ Files/Car\ Audio/Headphones/Fostex\ T40RP\ mk3\ new.trak"
+alias coreaudio="sudo launchctl kickstart -kp system/com.apple.audio.coreaudio; sudo killall 'AU Lab' >> /dev/null;"
+
 # Quick folder navigation
 alias desk="cd ~/Desktop"
 alias down="cd ~/Downloads"

dd08da20e86d56b01b713fb899227c261aa94e52
pyropoptrt <lee@pyropoptrt.com>
2023-02-02 00:34:17 -0600

    Linked to the proper folder

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,4 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-$PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+# $PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
+export PATH="/opt/homebrew/opt/coreutils/bin/:$PATH"

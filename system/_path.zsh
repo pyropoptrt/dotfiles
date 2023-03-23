cd65305d29653d0a283f9c58909acc3d493bc16a
pyropoptrt <pyropoptrt@gmail.com>
2023-03-23 15:43:30 -0500

    Added our path for composer on Zorin/Ubuntu/Debian desktop

--- system/_path.zsh
+++ system/_path.zsh
@@ -2,3 +2,6 @@ export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 # $PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
 export PATH="/opt/homebrew/opt/coreutils/bin/:$PATH"
+
+# composer path on linux desktops (at least on zorin)
+export PATH="/home/${USER}/.config/composer/vendor/bin:$PATH";

482366fa68bb25dbffb680d8cc9a6357cd52ef88
pyropoptrt <lee@pyropoptrt.com>
2023-10-18 09:55:11 -0500

    run our dist-upgrade as a different user

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -48,6 +48,7 @@ alias dnsmasq="vim ~/.config/valet/dnsmasq.d/tld-test.conf && sudo brew services
 alias ans="cd ~/Documents/ansible"
 alias ap="ans; ansible-playbook -K"
 alias dist="ans; ap ubuntu-dist-upgrade.yaml"
+alias distu="ans; ap -u unixadmin ubuntu-dist-upgrade.yaml"
 alias yum="ans; ap centos-ossec_aide-update.yaml"
 
 

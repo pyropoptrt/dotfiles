86d8ad3c8e45b9d314070ad35376654bd8ff6b61
pyropoptrt <lee@pyropoptrt.com>
2023-10-18 09:54:32 -0500

    Converted from tabs to spaces

--- functions/networking.zsh
+++ functions/networking.zsh
@@ -33,48 +33,49 @@ function ns {
 }
 
 
+
 # Get the geo location for an ip address
 function geoip {
-	curl http://api.db-ip.com/v2/free/$@;
+    curl http://api.db-ip.com/v2/free/$@;
 }
 
 # timing script for connection, start transfer, and total time
 function response {
-	# echo "connect | start | total";
+    # echo "connect | start | total";
 
-	curl_format='{
-		"connect": %{time_connect},
-		"start transfer": %{time_starttransfer},
-		--------------------------------------------------
-		"total time": %{time_total}
-	}';
+    curl_format='{
+        "connect": %{time_connect},
+        "start transfer": %{time_starttransfer},
+        --------------------------------------------------
+        "total time": %{time_total}
+    }';
 
-	curl -o /dev/null -w "${curl_format}" -s $@;
+    curl -o /dev/null -w "${curl_format}" -s $@;
 }
 
 # Get overall timing
 function timing {
 
-	#
-	# curl wrapper returning timing information.
-	#
-	# curl format adapted from
-	#  http://josephscott.org/archives/2011/10/timing-details-with-curl/
-	#
-	# Example usage:
-	#   $ timing http://www.apple.com
-	#   $ time timing http://www.apple.com -v
+    #
+    # curl wrapper returning timing information.
+    #
+    # curl format adapted from
+    #  http://josephscott.org/archives/2011/10/timing-details-with-curl/
+    #
+    # Example usage:
+    #   $ timing http://www.apple.com
+    #   $ time timing http://www.apple.com -v
 
-	curl_format='{
-	 "time_namelookup": %{time_namelookup},
-	 "time_connect": %{time_connect},
-	 "time_appconnect": %{time_appconnect},
-	 "time_pretransfer": %{time_pretransfer},
-	 "time_redirect": %{time_redirect},
-	 "time_starttransfer": %{time_starttransfer},
-	  --------------------------------------------------
-	 "time_total": %{time_total}
-	}';
+    curl_format='{
+     "time_namelookup": %{time_namelookup},
+     "time_connect": %{time_connect},
+     "time_appconnect": %{time_appconnect},
+     "time_pretransfer": %{time_pretransfer},
+     "time_redirect": %{time_redirect},
+     "time_starttransfer": %{time_starttransfer},
+      --------------------------------------------------
+     "time_total": %{time_total}
+    }';
 
-	curl -w "${curl_format}" -o /dev/null -s $@;
+    curl -w "${curl_format}" -o /dev/null -s $@;
 }
